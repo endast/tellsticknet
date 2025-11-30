2fe1c98babd4cb1ebc9143192097fd022c9924f6
Magnus Wahlberg <endast@gmail.com>
2025-11-30 11:00:25 +0100

    Fix version

--- tellsticknet/__init__.py
+++ tellsticknet/__init__.py
@@ -2,9 +2,10 @@
 
 import logging
 import sys
+from importlib.metadata import version
 
 assert sys.version_info >= (3, 0)
 
 _LOGGER = logging.getLogger(__name__)
 
-__version__ = "0.0.1"
+__version__ = version("tellsticknet")
